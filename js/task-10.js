d2ca36fc4b959aaaba067b2b271bb6b8fcb6209c
ViktoriaReminna <121427776+ViktoriaReminna@users.noreply.github.com>
2023-06-08 16:44:57 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,29 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controlsEl = document.getElementById('controls');
+
+const inputEl = controlsEl.firstElementChild;
+
+const createBtn = controlsEl.querySelector('[data-create]');
+
+const destroyBtn = controlsEl.querySelector('[data-destroy]');
+
+const divBoxes = document.getElementById('boxes');
+
+const createBoxes = amount => {
+  for (let i = 0; i < amount; i++) {
+    const divEl = document.createElement('div');
+    divEl.style.backgroundColor = getRandomHexColor();
+    divEl.style.width = '30px';
+    divEl.style.height = '30px';
+    divBoxes.appendChild(divEl);
+  }
+};
+
+const onCreateBtnClick = () => {
+  createBoxes(inputEl.value);
+};
+
+createBtn.addEventListener('click', onCreateBtnClick);
